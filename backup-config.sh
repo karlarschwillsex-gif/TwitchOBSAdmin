2ac3170820559ff9f0c98653c536db51060cf2ec
Phynnias “Fynn” Logan <karlarschwillsex@gmail.com>
2026-04-02 18:44:30 +0200

    Delete backup-config.sh

--- backup-config.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-tar -czf /home/fynn/backups/twitchobsadmin-config-$(date +%F-%H%M).tar.gz /home/fynn/TwitchOBSAdmin --exclude='/home/fynn/TwitchOBSAdmin/node_modules'
-find /home/fynn/backups -type f -mtime +30 -delete
