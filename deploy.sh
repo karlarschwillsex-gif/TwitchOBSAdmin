4a0b77621295e726f2a05524e382ecc971dc2d2a
Phynnias “Fynn” Logan <karlarschwillsex@gmail.com>
2026-04-02 18:47:01 +0200

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd /home/fynn/TwitchOBSAdmin
-git pull origin main
-npm ci --production
-sudo systemctl restart server.service eventsub.service credits.service bot.service
